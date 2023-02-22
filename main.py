5a7f629a4a39d27e7d2a395343778b0975bcf463
Kori2303 <125811456+Kori2303@users.noreply.github.com>
2023-02-22 09:34:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+a = int(input("Введите первое число\t"))
+b = int(input("Введите первое число\t"))
+print((a-b)*(a+b))
+print(a^2-2*a*b+b^2)
+print(a^2+2*a*b+b^2)
+print(a^3-3*a^2*b+3*a*b^2-b^3)
+print(a^3+3*a^2*b+3*a*b^2+b^3)
+print((a+b)*(a^2-a*b+b^2))
+print((a-b)*(a^2+a*b+b^2))
+
+
+
+
+
+
